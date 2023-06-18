ed28a9904a4f98abd1e945a9827167799c37d7b4
Gyula Németh <gyula@chamaileon.io>
2023-06-18 12:53:29 +0200

    main page + waitlist signup

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,17 +1,86 @@
 import { defineConfig } from 'vitepress'
 
+
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "bluefox.email",
   description: "High deliverability & brand consistency.",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: '/assets/bluefoxemail-logo.png',
+    head: [
+      ['script', { src: 'https://www.googletagmanager.com/gtag/js?id=G-RFX7RXXS7C' }],
+      ['script', {}, `window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-RFX7RXXS7C');`]
+    ],
     nav: [
+      /*
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Email Marketing Concepts', link: '/email-marketing-concepts/' },
+      { text: 'Tutorials', link: '/tutorials/' },
+      { text: 'About', link: '/about' }
+      */
     ],
 
     sidebar: [
+      {
+        text: 'Email Marketing Concepts',
+        link: '/email-marketing-concepts/'
+      },
+      {
+        text: 'Tutorials',
+        link: '/tutorials/',
+        collapsed: true,
+        items: [
+          {
+            text: 'Email Marketing 101',
+            link: '/tutorials/email-marketing-101/',
+            collapsed: true,
+            items: [
+              {
+                text: 'Getting Started with Building Your List',
+                link: '/tutorials/email-marketing-101/getting-started-with-building-your-list'
+              },
+              {
+                text: 'Crafting Compelling Emails',
+                link: '/tutorials/email-marketing-101/crafting-compelling-emails-the-art-of-effective-copywriting'
+              },
+              {
+                text: 'Unlocking the Power of Personalization',
+                link: '/tutorials/email-marketing-101/unlocking-the-power-of-personalization-segmentation-and-dynamic-content'
+              },
+              {
+                text: 'Designing Engaging Emails',
+                link: '/tutorials/email-marketing-101/designing-engaging-emails-templates-layouts-and-visual-elements'
+              },
+              {
+                text: 'Mastering Subject Lines',
+                link: '/tutorials/email-marketing-101/mastering-subject-lines-strategies-for-higher-open-rates'
+              },
+              {
+                text: 'Automation Demystified',
+                link: '/tutorials/email-marketing-101/automation-demistified-nurturing-leads-with-automated-email-campaigns'
+              },
+              {
+                text: 'Maximizing Clicks and Conversions',
+                link: '/tutorials/email-marketing-101/maximizing-clicks-and-conversions-optimizing-ctas-and-landing-pages'
+              },
+              {
+                text: 'Analyzing Email Performance',
+                link: '/tutorials/email-marketing-101/analyzing-email-performance-metrics-and-insights-for-success'
+              },
+              {
+                text: 'Deliverability Matters',
+                link: '/tutorials/email-marketing-101/deliverability-matters-best-practices-for-inbox-placement'
+              },
+              {
+                text: 'Growing and Maintaining a Healthy Email List',
+                link: '/tutorials/email-marketing-101/growing-and-maintaining-a-healthy-email-list-list-hygene-and-complience'
+              }
+            ]
+          }
+        ]
+      },
       {
         text: 'Examples',
         items: [
@@ -22,7 +91,10 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      { icon: 'twitter', link: 'https://twitter.com/gyulanemeth85' }
+    ],
+    footer: {
+      copyright: 'Copyright © 2023-present GYN Technologies OÜ'
+    }
   }
 })
--- index.md
+++ index.md
@@ -5,21 +5,23 @@ layout: home
 hero:
   name: "bluefox.email"
   text: "High deliverability & brand consistency."
-  tagline: My great project tagline
+  tagline: What does the fox mail?
+  image: '/assets/bluefoxemail-logo.png'
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
-    - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: Join Our Waitlist
+      link: /signup
+
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Beautiful & Consistent Designs
+    details: Our design systems ensures consistent branding and visual identity across email campaigns, creating a professional and cohesive brand image. Additionally, a design system streamlines the email creation process, saving time and effort by providing pre-designed components and templates that can be easily reused, resulting in faster and more efficient email production.
+    icon: 🎨
+  - title: Incredible Deliverability with AWS SES
+    details: Harness the unmatched power of AWS SES for superior email delivery. With its scalable and reliable platform, backed by Amazon's renowned reputation for deliverability, you can confidently reach your audience's inboxes. Maximize engagement and effectively communicate your brand's message with AWS SES, ensuring high deliverability rates and achieving your marketing goals with ease.
+    icon: 📨
+  - title: Detailed Built-in Analytics
+    details: Unleash the power of our robust built-in analytics to elevate your email marketing campaigns. Gain valuable insights into key performance metrics, track engagement, and make data-driven decisions to maximize the impact of your marketing efforts. Drive targeted strategies, improve conversions, and achieve unparalleled success in your email marketing endeavors.
+    icon: 📊
 ---
 
