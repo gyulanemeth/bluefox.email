6d9eca4fa870295e78a5498e1bb3d22161466584
Gyula Németh <gyula@chamaileon.io>
2025-04-10 23:10:45 +0200

    defineProps is a compiler marco, no need to import

--- .vitepress/theme/DesignSystem.vue
+++ .vitepress/theme/DesignSystem.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, ref, onMounted, onBeforeUnmount } from 'vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import DesignSystemLeftSvg from './DesignSystemLeftSvg.vue'
 import DesignSystemMiddleSvg from './DesignSystemMiddleSvg.vue'
 import DesignSystemRightSvg from './DesignSystemRightSvg.vue'
