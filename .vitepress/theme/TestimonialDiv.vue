b0f20de8d99394017ec25514bf053a000400d4db
243mjparth@gmail.com <243mjparth@gmail.com>
2025-05-28 10:48:31 +0530

    Update testimonial for Nicole Darechuk and add local package reference

--- .vitepress/theme/TestimonialDiv.vue
+++ .vitepress/theme/TestimonialDiv.vue
@@ -75,7 +75,7 @@ const testimonials = [
     name: 'Nicole Darechuk',
     initials: 'ND',
     company: 'Consultant',
-    testimonial: ` One of the best I've had in a long time working with an email marketing platform. I love how easy it is to configure branding elements for repurposing. I also love the campaign configuration and setup of new campaigns is so simple. `,
+    testimonial: ` One of the best I've had in a long time working with an email marketing platform. I love how easy it is to configure branding elements for repurposing. I also love the campaign configuration and setup of new campaigns is so simple. Add to that, the support to get setup was above and beyond. I would highly recommend! `,
     profileImg: '/assets/testimonials/ND.webp',
     logo: '/assets/testimonials/NDcompany.webp',
     logoAlt: 'ND Logo',
