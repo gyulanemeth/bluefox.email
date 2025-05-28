067fd26a064afb38ff32e235f4b837c464ec05e6
243mjparth@gmail.com <243mjparth@gmail.com>
2025-05-28 16:36:41 +0530

    Fix company name for Nicole Darechuk in testimonials section and update package-lock.json

--- .vitepress/theme/TestimonialDiv.vue
+++ .vitepress/theme/TestimonialDiv.vue
@@ -74,7 +74,7 @@ const testimonials = [
     id: 4,
     name: 'Nicole Darechuk',
     initials: 'ND',
-    company: 'Consultant @NDCONSULTING',
+    company: 'Consultant @Ndconsulting',
     testimonial: ` One of the best I've had in a long time working with an email marketing platform. I love how easy it is to configure branding elements for repurposing. I also love the campaign configuration and setup of new campaigns is so simple. Add to that, the support to get setup was above and beyond. I would highly recommend! `,
     profileImg: '/assets/testimonials/ND.webp',
     logo: '/assets/testimonials/NDcompany.webp',
