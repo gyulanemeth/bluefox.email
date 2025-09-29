f96be9deb6ed5664797b29b96738e40c75b084da
tiwariParth <243mjparth@gmail.com>
2025-09-29 17:27:16 +0530

    typo solve in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 // In tailwind.config.js
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    // This tells Tailwind to scan all your Vue components and Markdown files
     "./.vitepress/theme/**/*.vue",
     "./**/*.md",
   ],
