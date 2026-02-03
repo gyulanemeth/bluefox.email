7d8896b1594d8b0b5325885efb6eb7611565a51f
AhmedQudaih <ahmedku0123@gmail.com>
2026-02-03 10:04:24 +0200

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -26,6 +26,7 @@ if (env.VITE_APP_ENV === 'production') {
     `window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-RFX7RXXS7C');\ngtag('config', 'AW-16693655873');`,
   ])
 }
+// add under the if condition before going to prod
 headConf.push([
   "script",
   {},
